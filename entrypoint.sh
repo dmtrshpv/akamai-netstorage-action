3b5678285037bb74f265caae017eba0a35418dc4
Dale Lenard <dale_lenard@hotmail.com>
2020-09-15 20:13:38 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,15 @@ set -e
 set -o pipefail
 
 # Build Jekyll
-
-
+bundle install
+bundle exec jekyll build
 
 # Create /root/.edgerc file from env variable
 echo -e "${EDGERC}" > /root/.edgerc
 
 # Upload to NetStorage
 akamai netstorage \
-  --edgerc /root/.edgerc \
+  --config /root/.edgerc \
   --section netstorage \
+  put --from ./_site/ /886532/_site
   
