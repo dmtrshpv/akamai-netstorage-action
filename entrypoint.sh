d0b03c70d858ce71df1aa650a092b9afb82e93e6
Dale Lenard <dale_lenard@hotmail.com>
2020-09-16 17:52:14 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ domainName=$3
 
 # Create private_key file from env variable
 echo -e "${AKAMAI_PRIVATEKEY}" > /root/privatekey
-
+chmod 600 /root/privatekey
 # Upload to NetStorage
 scp -i /root/privatekey -oHostKeyAlgorithms=+ssh-dss -r /github/workspace/${path} sshacs@${domainName}.scp.upload.akamai.com:/${cpCode}/${path}/
   
