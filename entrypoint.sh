e079875a1867b4e99d91bac2f56927b8e3eeb954
Dale Lenard <dale_lenard@hotmail.com>
2020-09-16 19:45:43 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,14 @@ domainName=$3
 /bin/bash -l -c "bundle install"
 /bin/bash -l -c "bundle exec jekyll build"
 
+#Authorise SSH host
+mkdir -p /root/.ssh
+chmod 0700 /root/.ssh
+ssh-keyscan ${domainName}.scp.upload.akamai.com > /root/.ssh/known_hosts
+
 # Create private_key file from env variable
-echo -e "${AKAMAI_PRIVATEKEY}" > /root/privatekey
-chmod 600 /root/privatekey
+echo -e "${AKAMAI_PRIVATEKEY}" > /root/.ssh/privatekey
+chmod 600 /root/.ssh/privatekey
 # Upload to NetStorage
 scp -i /root/privatekey -o 'HostKeyAlgorithms=+ssh-dss' -o 'StrictHostKeyChecking no' -r /github/workspace/${path} sshacs@${domainName}.scp.upload.akamai.com:/${cpCode}/${path}/
   
