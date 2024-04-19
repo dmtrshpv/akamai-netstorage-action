1bd5b792e1a565c3041111ea729bd36884810bbb
DimiterShopov <shd2mu@bosch.com>
2024-04-19 14:00:00 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,5 +16,6 @@ echo "${AKAMAI_PRIVATEKEY}" > /root/.ssh/privatekey
 chmod 600 /root/.ssh/privatekey
 
 # Upload to NetStorage
+echo "wassup"
 scp -i /root/.ssh/privatekey -o 'HostKeyAlgorithms=+ssh-dss' -o 'StrictHostKeyChecking no' -r /github/workspace/${path}/ sshacs@${domainName}.scp.upload.akamai.com:/${cpCode}/${nspath}/  
   
